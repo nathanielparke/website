b49fe316a40b068f9e5a3e3b9ae2e60d84567acb
Vishal Kalyanasundaram <vishalkal@berkeley.edu>
2014-06-16 18:59:40 -0400

    Adds vice chairs

--- Committees.php
+++ Committees.php
@@ -15,18 +15,21 @@
             <div class="col-md-6 committee">
                 <h3 class="committee-heading"> 1st Disarmament & International Security</h3>
                 <h5><strong>Head Chair: Nate Parke</strong></h5>
+                <h5><strong>Vice Chairs: Sarah Yue, Paul Lee</h5></strong>
                 <h5><strong>Topics: Cyber Warfare, Illicit Substance Trade</strong></h5>
             </div>
 
             <div class="col-md-6 committee" style="text-align:right">
                 <h3 class="committee-heading"> 3rd Social, Cultural & Humanitarian</h3>
                 <h5><strong>Head Chair: Kim Nguyen</strong></h5>
+                <h5><strong>Vice Chairs: Benjy Malings, Emily Reece</h5></strong>
                 <h5><strong>Topics: Self-Determination, Ethics of Technology</strong></h5>
             </div>
 
             <div class="col-md-6 committee" style="text-align:left">
                 <h3 class="committee-heading"> 4th Special Political & Decolonization</h3>
                 <h5><strong>Head Chair: Gabby Armato</strong></h5>
+                <h5><strong>Vice Chairs: Gloria Cheung, Vin Ganeshan</h5></strong>
                 <h5><strong>Topics: Peacekeeping, Palestinian Refugees and Human Rights</strong></h5>
             </div>
 
@@ -37,106 +40,123 @@
             <div class="col-md-6 committee">
                 <h3 class="committee-heading"> Interpol</h3>
                 <h5><strong>Head Chair: Arshia Singh</strong></h5>
+                <h5><strong>Vice Chairs: David McDonogh, Eric Dumas</h5></strong>
                 <h5><strong>Topics: Sports Policing, Maritime Piracy</strong></h5>
             </div>
 
             <div class="col-md-6 committee" style="text-align:right">
                 <h3 class="committee-heading"> Food and Agricultural Organization</h3>
                 <h5><strong>Head Chair: Sharon Licht</strong></h5>
+                <h5><strong>Vice Chairs: Giselle Fang, Winnie Itago</h5></strong>
                 <h5><strong>Topics: Sustainable Land Management, Food Insecurity</strong></h5>
             </div>
 
             <div class="col-md-6 committee">
                 <h3 class="committee-heading"> World Health Organization</h3>
                 <h5><strong>Head Chair: Katie Hilton</strong></h5>
+                <h5><strong>Vice Chairs: Thanh Mai Bercher</h5></strong>
                 <h5><strong>Topics: Regulation of Antibiotic/Drug Administration, Treatment of Disabilities in Developing Nations</strong></h5>
             </div>
 
             <div class="col-md-6 committee" style="text-align:right">
                 <h3 class="committee-heading"> UN Environmental Programme</h3>
                 <h5><strong>Head Chair: Manu Vaish</strong></h5>
+                <h5><strong>Vice Chairs: Victoria Peng, Kristen Gallogly</h5></strong>
                 <h5><strong>Topics: Climate Change Induced Migration, Oceanic Pollution with Emphasis on Hypoxic Dead Zones</strong></h5>
             </div>
 
             <div class="col-md-6 committee">
                 <h3 class="committee-heading"> International Labour Organization</h3>
                 <h5><strong>Head Chair: Shilpa Daulat</strong></h5>
+                <h5><strong>Vice Chairs: Efe Atli, Nikhil Narayen</h5></strong>
                 <h5><strong>Topics: Maintaining the Rights of Farm Workers, Promotion of Cooperatives to Drive Job Growth</strong></h5>
             </div>
 
             <hr>
 
-            <h2 class="bloc-heading">Specialized</h2>
+            <h2 class="bloc-heading">Specialized<small> Crisis Manager: Paige Xio Alvarez</small></h2>
 
             <div class="col-md-6 committee">
                 <h3 class="committee-heading"> European Union</h3>
                 <h5><strong>Head Chair: Matt Nguyen</strong></h5>
+                <h5><strong>Vice Chairs: Danny Tuthill</h5></strong>
                 <h5><strong>Topics: Foreign Relations with the Russian Federation, The European Year for Development 2015</strong></h5>
             </div>
 
             <div class="col-md-6 committee" style="text-align:right">
                 <h3 class="committee-heading"> Asia-Pacific Economic Cooperation</h3>
                 <h5><strong>Head Chair: Nicola Evans</strong></h5>
+                <h5><strong>Vice Chairs: Mischa Fritz</h5></strong>
                 <h5><strong>Topics: Standardizing Emergency Response to Natural Disasters, Regulating Economic Policies to Preserve Human Security</strong></h5>
             </div>
 
             <div class="col-md-6 committee">
                 <h3 class="committee-heading"> Group of 20</h3>
                 <h5><strong>Head Chair: Jai Malhotra</strong></h5>
+                <h5><strong>Vice Chairs: Pranay Patil</h5></strong>
                 <h5><strong>Topics: Currency Wars, Brain Drain: Human Capital</strong></h5>
             </div>
 
             <div class="col-md-6 committee" style="text-align:right">
                 <h3 class="committee-heading"> International Criminal Court</h3>
                 <h5><strong>Head Chair: Maria Salamanca</strong></h5>
+                <h5><strong>Vice Chairs: Liz Zarka</h5></strong>
                 <h5><strong>Topics: Colombia, Palestine v. Israel</strong></h5>
             </div>
 
             <div class="col-md-6 committee" >
                 <h3 class="committee-heading"> Security Council</h3>
                 <h5><strong>Head Chair: Brandon Doan</strong></h5>
+                <h5><strong>Vice Chairs: Trevor Dowds</h5></strong>
                 <h5><strong>Topics: South Sudan <small>(Subject to Change)</small>, Open Agenda</strong></h5>
             </div>
 
             <div class="col-md-6 committee" style="text-align:right">
                 <h3 class="committee-heading"> Inter-American Development Bank</h3>
                 <h5><strong>Head Chair: Emily Truax</strong></h5>
+                <h5><strong>Vice Chairs: Brett Buckingham, Hunter Conrad</h5></strong>
                 <h5><strong>Topics: International Financial Institutions, Early Exposure to Technology in Latin America</strong></h5>
             </div>
 
             <div class="col-md-6 committee">
                 <h3 class="committee-heading"> United Nations Global Compact</h3>
                 <h5><strong>Head Chair: Kevin Kai</strong></h5>
+                <h5><strong>Vice Chairs: Jonas Majewski</h5></strong>
                 <h5><strong>Topics: Strategic Expansion of Businesses, Regulating Globalization to Improve Local Business</strong></h5>
             </div>
 
             <div class="col-md-6 committee" style="text-align:right">
                 <h3 class="committee-heading"> International Finance Cooperation</h3>
                 <h5><strong>Head Chair: Sergio-Ruiz Tello</strong></h5>
+                <h5><strong>Vice Chairs: Vishal Kalyanasundaram, Dylan Randle</h5></strong>
                 <h5><strong>Topics: Open Agenda on Investment Climate & Public-Private Partnerships</strong></h5>
             </div>
 
             <div class="col-md-6 committee">
                 <h3 class="committee-heading"> Genghis Khan's Military Dynasty</h3>
                 <h5><strong>Head Chair: Riyaz Shah</strong></h5>
+                <h5><strong>Vice Chairs: Akshay Jagadeesh</h5></strong>
                 <h5><strong>Topics: Mongol Expansion</strong></h5>
             </div>
 
             <div class="col-md-6 committee" style="text-align:right">
                 <h3 class="committee-heading"> Atlantic City Conference</h3>
                 <h5><strong>Head Chair: Gabi Espinoza</strong></h5>
+                <h5><strong>Vice Chairs: Hee Soo Kim, David Ekstrom</h5></strong>
                 <h5><strong>Topics: Prohibition, Chicago Bootleg Wars</strong></h5>
             </div>
 
             <div class="col-md-6 committee">
                 <h3 class="committee-heading"> Joint Crisis Cabinet</h3>
                 <h5><strong>Head Chair: Andy Yuan</strong></h5>
+                <h5><strong>Vice Chairs: Rita Hu, Tiffany Gean</h5></strong>
                 <h5><strong>Topics: Chinese Civil War</strong></h5>
             </div>
 
             <div class="col-md-6 committee" style="text-align:right">
                 <h3 class="committee-heading"> Press Corps</h3>
                 <h5><strong>Head Chair: Amanda Lee/Alex Feibleman</strong></h5>
+                <h5><strong>Vice Chairs: Isabel Viegas de Lima</h5></strong>
                 <h5><strong>Topics: Open Agenda</strong></h5>
             </div>
 
