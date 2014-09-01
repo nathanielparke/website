0c2439bd92ced739ec17e8b00f6f83867176df86
Vishal Kalyanasundaram <vishalkal@berkeley.edu>
2014-09-01 12:59:33 -0700

    Timeline code

---
+++ Timeline.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Berkeley Model UN">
+        <meta name="description" content="Berkeley Model United Nations">
+        <meta name="keywords" content="Berkeley, BMUN, Model UN, MUN, Model United Nations, United Nations">
+        <title>Berkeley Model United Nations</title>
+        <link rel="icon" type="img/png" href="img/favicon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.css"  type="text/css"/>
+
+        <!-- Custom Styles -->
+        <link rel="stylesheet" href="css/vertical_timeline.css" type="text/css">
+        <link rel="stylesheet" href="css/bmun-theme.css" type="text/css"/>
+    </head>
+
+    <body> 
+        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+
+<?php 
+    include 'html/navbar.html'
+?>
+
+<div class="container">
+    <div class="col-md-10 col-md-offset-1 content bgimg">
+    	<h1 class="content-heading">BMUN LXIII Timeline</h1>
+        <hr>
+
+        <section id="cd-timeline" class="cd-container">
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-content">
+					<h2>First Mailing</h2>
+					<span class="cd-date">July 21</span>
+				</div> 
+			</div> 
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-content">
+					<h2>Reminder Email About Registration</h2>
+					<span class="cd-date">Sep 14</span>
+				</div> 
+			</div> 
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-content">
+					<h2>Registration Begins</h2>
+					<span class="cd-date">Sep 15</span>
+				</div> 
+			</div>
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-content">
+					<h2>Delegate Workshop</h2>
+					<span class="cd-date">Nov 1</span>
+				</div> 
+			</div> 
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-content">
+					<h2>Second Mailing; Topic Synopses; First round of Country Assignments</h2>
+					<span class="cd-date">Nov 2/3</span>
+				</div> 
+			</div> 
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-content">
+					<h2>Third Mailing</h2>
+					<span class="cd-date">Dec 25/26</span>
+				</div> 
+			</div>
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-content">
+					<h2>Guaranteed Deadlines</h2>
+					<span class="cd-date">Jan 17</span>
+				</div> 
+			</div>
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-content">
+					<h2>Assignment Finalization</h2>
+					<span class="cd-date">Jan 24</span>
+				</div> 
+			</div> 
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-content">
+					<h2>Delegate Name Finalization; Waitlist Assignment Finalization</h2>
+					<span class="cd-date">Feb 6</span>
+				</div> 
+			</div> 
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-content">
+					<h2>Fourth Mailing</h2>
+					<span class="cd-date">Feb 7/8</span>
+				</div> 
+			</div>
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-content">
+					<h2>Delegate Name Finalization For Waitlisted Schools; Delegate Waivers Due</h2>
+					<span class="cd-date">Feb 13</span>
+				</div> 
+			</div>
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-content">
+					<h2>Delegate Guide and Advisor Checklist</h2>
+					<span class="cd-date">Feb 16</span>
+				</div> 
+			</div> 
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-content">
+					<h2>BMUN LXIII</h2>
+					<span class="cd-date">Feb 27-Mar 1</span>
+				</div> 
+			</div> 
+
+		</section>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    </div>
+</div>
+
+<?php
+    include 'html/footer.html';
+?>
