88b029b32adba50031015432887b811e978f3882
Vishal Kalyanasundaram <vishalkal@berkeley.edu>
2014-09-30 12:05:28 -0700

    Adds mailings

---
+++ Mailings.php
@@ -0,0 +1,76 @@
+<?php 
+    include 'html/header.html';
+    include 'html/navbar.html'
+?>
+
+<div class="container">
+    <div class="col-md-10 col-md-offset-1 content">
+        <h1 class="content-heading">Mailings</h2>
+        <hr>
+
+        <div class="panel-group" id="accordion">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+				  <h4 class="panel-title">
+				    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+				      July 21st - First Mailing
+				    </a>
+				  </h4>
+				</div>
+				<div id="collapseOne" class="panel-collapse collapse">
+				  <div class="panel-body">
+				    <p> Dearest MUN Advisors, </p>
+					<p>On behalf of our secretariat and our Secretary-General Shrey Goel, I would like to cordially invite you to rejoin us from <b>February 27th to March 1st of 2015</b> on the beautiful and historic UC Berkeley campus to participate in BMUN LXIII. This coming 63rd session is comprised of a diverse array of twenty committees, including the bilingual <i>Inter-American Development Bank</i>, <i>Interpol</i>, and the return of our very own <i>Press Corps</i> committee.</p>
+					<p>Attached are links to three documents for your perusal:</p>
+					<ol>
+						<li><a href="SG_Letter.php">A letter of welcome from our Secretary-General</a></li>
+						<li><a href="Golden_Bears_Letter.php">Golden Bears Letter</a> written by our Chief of Staff Riyaz Shah for incoming UC Berkeley freshmen who would like to join us. If interested, they can sign up for our recruitment mailing list here</li>
+						<li><a href="Outreach_Letter.php">Outreach Letter</a> that details our community initiatives from our Under-Secretary-General of Outreach Gabriella Espinoza</li>
+					</ol>
+					<p>This year, registration opens on <b>Monday, September 15</b>. We strongly encourage advisors to register their schools as soon as possible to ensure that they receive the country assignments they would like. More information on the registration process will be provided as we approach September. If you have any questions at all regarding BMUN LXIII, please do not hesitate to contact me at info@bmun.org. </p>
+
+					<p>Thank you, and we look forward to your participation in BMUN next February.</p>
+
+					<p style="align:left; max-width:50%">Hee Soo Kim<br>
+					USG of External Relations<br>
+					Berkeley Model United Nations, 63rd Session<br>
+					<a href="mailto:info@bmun.org" target="_blank">info@bmun.org</a></p>
+
+				  </div>
+				</div>
+			</div>
+		  
+			<div class="panel panel-default">
+				<div class="panel-heading">
+				  <h4 class="panel-title">
+				    <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+				      Sep 15th - Registration Mailing
+				    </a>
+				  </h4>
+				</div>
+				<div id="collapseTwo" class="panel-collapse collapse">
+				  <div class="panel-body">
+				    <p>Dear MUN Advisor,</p>
+					<p>We are happy to announce that our registration application is now officially open. You can go to our website and find our registration application under the ‘Advisors’ tab. A full list of our committees can be viewed on our website also, <a href="Committees.php">here</a>, and questions about committees can be answered by viewing our Advisor FAQs page <a href="Advisor_FAQs.php">here</a>.</p>
+					<p>This year at BMUN, we will be performing registration on a first-come-first serve basis. In other words, as soon as a school registers, they will be automatically assigned countries and spots in specialized committees based on the preferences they indicate on the registration page. This is to accommodate a growing constituency of schools and delegates from around the globe and to provide all schools with a fair opportunity to get the spots they want.</p>
+					<p>A timeline of our important dates, including when the first round of country assignments will be released, is available on our website <a href="Timeline.php">here</a>.</p>
+
+					<p>Thank you, and we look forward to having you at BMUN LXIII.</p>
+
+					<p style="align:left; max-width:50%">Hee Soo Kim<br>
+					USG of External Relations<br>
+					Berkeley Model United Nations, 63rd Session<br>
+					<a href="mailto:info@bmun.org" target="_blank">info@bmun.org</a></p>
+				  </div>
+				</div>
+			</div>
+		  
+		</div>
+
+	</div>
+</div>
+
+
+<?php
+    include 'html/footer.html';
+?>
