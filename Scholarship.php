77bd811022350bf37632394f0fb120564bf03624
Vishal Kalyanasundaram <vishalkal@berkeley.edu>
2014-12-15 19:13:16 -0800

    Alumni Scholarship

---
+++ Scholarship.php
@@ -0,0 +1,30 @@
+<?php 
+    include 'html/header.html';
+    include 'html/navbar.html'
+?>
+
+<!-- Marketing Container -->
+<div class="container">
+    <div class="col-md-10 col-md-offset-1 content bgimg">
+        <h1 class="content-heading">Golden Bears Letter</h1>
+        <hr>
+
+        <p>Dear Delegates and Advisors,</p>
+        
+        <p>On behalf of the Secretary General and the entire Secretariat, I would like to warmly welcome you to BMUN LXIII. My name is Jonas Majewski, and I have the privilege of serving as this year’s Treasurer. BMUN prides itself on being an affordable conference that gives every delegate the opportunity to attend and succeed, no matter what their background or financial situation. As such, we set aside a large portion of our funds every year for the Alumni Scholarship. </p>
+        <p>This scholarship is meant for students who would otherwise struggle with or be unable to pay for the cost of attending BMUN. Each application will be evaluated on a case-by-case basis and will either partially or fully fund the delegate’s trip to the conference. There is no upper limit on the award, but delegates chosen for the scholarship will be required to provide receipts and documentation for their expenditures. </p>
+        <p>We highly encourage all students in financial need to apply. Please submit the requested materials by January 31, 2014. In addition, we ask that the faculty member or advisor recommending you submit his form separately by the same deadline. If you have any questions do not hesitate to reach out to us, and we will be more than happy to assist you. </p>
+
+        <p> The application can be downloaded <a href='BMUN_Alumni_Scholarship.pdf'>here</a>.</p>
+
+        <p>Sincerely,</p>
+
+        <p style="align:left; max-width:50%">Jonas Majewski<br>
+        Treasurer | BMUN LXIII <br>
+        <a href="mailto:chief@bmun.org" target="_blank">treasurer@bmun.org</a></p>
+    </div>
+</div>
+
+<?php
+    include 'html/footer.html';
+?>
