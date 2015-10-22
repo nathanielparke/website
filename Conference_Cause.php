af0d1ec7a2f2af06d6880836a3635616ecdaf227
Nathaniel Parke <nathaniel.parke@gmail.com>
2015-10-21 19:46:31 -0700

    adds in conference cause

--- Conference_Cause.php
+++ Conference_Cause.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
     include 'html/header.html';
     include 'html/navbar.html'
 ?>
 
 <!-- Marketing Container -->
 <div class="container">
-    <div class="col-md-10 col-md-offset-1 content bgimg">
+    <!-- <div class="col-md-10 col-md-offset-1 content bgimg">
         <h1 class="content-heading">Conference Cause</h2>
         <hr>
         <div class="row front">
@@ -22,12 +22,61 @@
         <div class="row front">
             <div class="col-md-6">
                 <img class="featurette-image img-responsive" src="img/TFA_Map.jpg">
-            </div>            
+            </div>
             <div class="col-md-6">
                 <p>Teach for All is a growing network of 35 organizations with a shared vision of expanding educational opportunity in their countries. Each partner recruits and develops leaders to teach in their nations’ most high-need classrooms and to work throughout their lives to increase opportunity for kids. Teach For All works to increase partners’ progress and accelerate collective impact through capturing and spreading knowledge, fostering direct connections and learning across the network, accessing global resources for the benefit of the whole, and contributing to the leadership development of staff, participants and alumni across the network. Learn more at <a href="http://teachforall.org/">Teach For All</a>.</p>
             </div>
         </div>
 
+    </div> -->
+    <div class="col-md-10 col-md-offset-1 content bgimg">
+        <h1 class="content-heading">Conference Cause</h2>
+        <hr>
+        <p>This year, Berkeley Model United Nations is excited to announce a partnership with Living Goods in order to support Global Health Advancement.</p>
+        <p>Despite decades of effort and billions spent on traditional aid, about 7 million children will still die this year from mostly preventable causes. The world is short at least 4 million trained, equipped, and supported health providers who can help to solve the problem.</p>
+        <p>Living Goods and BRAC Community Health Promoters conduct home visits, educate households on essential health behaviors, diagnose and treat childhood illnesses, and support pregnancies and newborns. The model recoups 100% of the products costs, and the wholesale margin covers a portion of the costs of running the network, allowing the model to deliver robust impacts for between $1-2 per capita per year. Agents earn an income, keeping them active and motivated.</p>
+        <p><strong>What is Living Goods?</strong> Living Goods supports ‘Avon-like’ networks of micro entrepreneurs who go door-to-door teaching families how to improve their health and wealth, and selling life-changing products like basic treatments for malaria and diarrhea, clean cook stoves, solar lights, and healthy fortified foods. Living Goods is building a cost-effective solution for fighting poverty and disease in the developing world.</p>
+        <br>
+        <div class="row front">
+            <div class="col-md-6">
+                <img class="featurette-image img-responsive" src="img/LG1.jpg">
+            </div>
+            <div class="col-md-6">
+                <p><strong>Living Goods’ Broad Product Mix Drives Impact And Sustainability:</strong>
+                    <ul>
+                        <li>Maternal Health: oral contraceptives, condoms, prenatal vitamins, clean delivery kit, baby caps</li>
+                        <li>Childhood Diseases: ACTs, ORS and zinc, amoxicillin, deworming</li>
+                        <li>Nutrition: fortified porridges, micronutrient powders, vitamins</li>
+                        <li>Health Tools: solar lights, clean burning cook stoves, water filters, water treatments</li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+        <br>
+        <div class="row front">
+            <div class="col-md-6" style="vertical-align:middle">
+                <p><strong>What is Living Goods’ impact?</strong>
+                    <ul>
+                        <li>Improved access to diagnosis and treatment of childhood illness. Researchers observed a 17% increase in diarrhea treatment with ORS and Zinc.</li>
+                        <li>Better knowledge and behaviors related to malaria and diarrhea. Respondents in treatment areas had better health knowledge about malaria and diarrhea, were more likely to sleep under a treated bed net, and were more likely to treat their water before drinking it.</li>
+                        <li>Increase in childhood treatment follow-ups. During the trial, there was a 54% increase in follow up visits for children sick with malaria, pneumonia, or diarrhea. Follow up visits may drive treatment adherence, as well as effective targeting of the most at risk cases.</li>
+                        <li>Large increase in newborn visits in first 7 days. New mothers saw a 72% increase in-home visits to newborns in the first 7 days postnatal.</li>
+                        <li>Observed halo effect on drug quality. Research showed 50% lower ACT counterfeits in areas where Living Goods or BRAC Community Health Promoters were present, suggesting an important influence on quality of products distributed in the private market.</li>
+                        <li>Impact consistent across wealth strata. There was no difference in impact between “poor” and “less poor” house-holds — this counters the concern that a model requiring client payments might bias impact to higher income families.</li>
+                    </ul>
+                </p>
+            </div>
+             <div class="col-md-6">
+                <img class="featurette-image img-responsive" src="img/LG2.jpg">
+            </div>
+        </div>
+        <br>
+        <p><strong>How to get involved?</strong> During conference weekend, BMUN will once again be passing around donation jars in committee to support Living Goods. Prior to conference though, the best and easiest way is to spread the word. Please like and follow Living Goods on social media. Every like, follow, and share exponentially increases their presence in the world and helps move towards improving global health for all. Below are links to their social media.</p>
+        <a href="https://www.facebook.com/livinggoods.org">FaceBook</a> <br>
+        <a href="https://twitter.com/living_goods">Twitter</a> <br>
+        <a href="https://www.linkedin.com/company/living-goods">LinkedIn</a> <br>
+        <a href="https://vimeo.com/livinggoods">Vimeo</a> <br>
+        <a href="https://www.flickr.com/photos/livinggoods/">Flickr</a> <br>
     </div>
 </div>
 
