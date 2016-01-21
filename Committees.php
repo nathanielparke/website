ca69e4cba9a656e070be7e4653137a9898600ea1
Nathaniel Parke <nathaniel.parke@gmail.com>
2016-01-20 20:38:06 -0800

    TS

--- Committees.php
+++ Committees.php
@@ -12,21 +12,21 @@
             <h2 class="bloc-heading">BLOC A</h2>
 
 
-            <div class="col-md-6 committee">
+            <div class=" committee">
                 <h3 class="committee-heading"> 1st Disarmament & International Security</h3>
                 <h5><strong>Head Chair: Tiffany Gean</strong></h5>
                 <h5><strong>Vice Chairs: Eric Dumas, Sherry Guo, Michael Pollack </h5></strong>
                 <h5><a href="ComingSoon.php">Extremist Groups in sub-Saharan Africa, Disarmament of the Arctic Circle</a> | <a href="http://disc.blogs.bmun.org/">Blog</a>| <a href="mailto:chief@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee" style="text-align:right">
-                <h3 class="committee-heading"> 3rd Social, Cultural & Humanitarian</h3>
+            <div class=" committee" >
+                <h3 class="committee-heading"> 3rd Social, Humanitarian and Cultural</h3>
                 <h5><strong>Head Chair: Isabel Viegas de Lima</strong></h5>
                 <h5><strong>Vice Chairs: Octavio Farfan, Sarah Yue, Himaja Jangle </h5></strong>
-                <h5><a href="ComingSoon.php">Migrant Worker Rights, Evaluating the Impacts of Sex Tourism</a> |  <a href="http://sochum.blogs.bmun.org/">Blog</a> | <a href="mailto:research@bmun.org" target="_blank">Email</a></h5>
+                <h5><a href="topicSynopses/SOCHUM.pdf">Migrant Worker Rights, Evaluating the Impacts of Sex Tourism</a> |  <a href="http://sochum.blogs.bmun.org/">Blog</a> | <a href="mailto:research@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee" style="text-align:left">
+            <div class=" committee">
                 <h3 class="committee-heading"> 6th Legal</h3>
                 <h5><strong>Head Chair: Kimberly Nguyen</strong></h5>
                 <h5><strong>Vice Chairs: Itago Kangashi, Chelsea Evans, Trent Gomberg </h5></strong>
@@ -37,35 +37,35 @@
 
             <h2 class="bloc-heading">BLOC B</h2>
 
-            <div class="col-md-6 committee">
+            <div class=" committee">
                 <h3 class="committee-heading"> Food and Agricultural Organization</h3>
                 <h5><strong>Head Chair: Manu Vaish</strong></h5>
                 <h5><strong>Vice Chairs: Adam Umemoto, Emily Yan, Jacob Hands</h5></strong>
                 <h5><a href="topicSynopses/FAO.pdf">Climate Change-Induced Agricultural Disasters, Prevention of Food-borne Diseases</a> |  <a href="http://fao.blogs.bmun.org/">Blog</a> | <a href="mailto:mvaish@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee" style="text-align:right">
+            <div class=" committee" >
                 <h3 class="committee-heading"> World Health Organization</h3>
                 <h5><strong>Head Chair: Hunter Conrad</strong></h5>
                 <h5><strong>Vice Chairs: Stacey Dojiri, Nicholas Spelta, TJ Ford </h5></strong>
-                <h5><a href="topicSynopses/WHO.pdf">Most Effective Use of Aid: Malaria vs. HIV, Drug Addiction: Crime or Health Issue?</a> |  <a href="http://who.blogs.bmun.org/">Blog</a> | <a href="mailto:hconrad@bmun.org" target="_blank">Email</a></h5>
+                <h5><a href="topicSynopses/WHO.pdf">The Double Burden of Disease</a> |  <a href="http://who.blogs.bmun.org/">Blog</a> | <a href="mailto:hconrad@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee">
+            <div class=" committee">
                 <h3 class="committee-heading"> United Nations Human Rights Council</h3>
                 <h5><strong>Head Chair: Emily Reece</strong></h5>
                 <h5><strong>Vice Chairs: Katie Lee, Sita McGuire, Daksh Bhatia</h5></strong>
                 <h5><a href="topicSynopses/UNHRC.pdf">Gender Equity in Education, Electoral Violence in Africa</a> |  <a href="http://unhrc.blogs.bmun.org/">Blog</a> | <a href="mailto:ereece@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee" style="text-align:right">
+            <div class=" committee" >
                 <h3 class="committee-heading"> United Nations Development Programme</h3>
                 <h5><strong>Head Chair: Benjy Malings</strong></h5>
                 <h5><strong>Vice Chairs: David McDonogh, Amanda Lee, Patricia Midy</h5></strong>
                 <h5><a href="ComingSoon.php">Participation in Fair Elections, Sovereignty During Disaster Relief</a> |  <a href="http://undp.blogs.bmun.org/">Blog</a> | <a href="mailto:outreach@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee">
+            <div class=" committee">
                 <h3 class="committee-heading"> World Bank</h3>
                 <h5><strong>Head Chair: Jonas Majewski</strong></h5>
                 <h5><strong>Vice Chairs: Rob Purviance, Nicola Evans, Andy Luo</h5></strong>
@@ -76,81 +76,81 @@
 
             <h2 class="bloc-heading">Specialized</h2>
 
-            <div class="col-md-6 committee">
+            <div class=" committee">
                 <h3 class="committee-heading"> Security Council</h3>
                 <h5><strong>Head Chair: Trevor Dowds</strong></h5>
                 <h5><strong>Vice Chairs: Kendra Singh, Liam Campbell</h5></strong>
                 <h5><a href="topicSynopses/SC.pdf">The Syrian Civil War, Open Agenda</a> |  <a href="http://sc.blogs.bmun.org/">Blog</a> | <a href="mailto:tdowds@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-             <div class="col-md-6 committee" style="text-align:right">
+             <div class=" committee" >
                 <h3 class="committee-heading"> International Court of Justice</h3>
                 <h5><strong>Head Chair: Liz Zarka</strong></h5>
                 <h5><strong>Vice Chairs: Jake Moskowitz, Colin Garcia, Michael McDonald</h5></strong>
                 <h5><a href="topicSynopses/ICJ.pdf">Russia v. Ukraine</a> | <a href="http://icj.blogs.bmun.org/">Blog</a>  | <a href="mailto:lzarka@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee">
+            <div class=" committee">
                 <h3 class="committee-heading"> Chinese State Council (Chinese Bilingual)</h3>
                 <h5><strong>Head Chair: Rita Hu</strong></h5>
                 <h5><strong>Vice Chairs: Haochen Zhou, Lucia Zhang</h5></strong>
                 <h5><a href="ComingSoon.php">Unrest in the West: Tibet & Xianjiang (Chinese), Foreign Investment in Latin America (English)</a> |  <a href="http://csc.blogs.bmun.org/">Blog</a> | <a href="mailto:info@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee" style="text-align:right">
+            <div class=" committee" >
                 <h3 class="committee-heading"> Asian Infrastructure Investment Bank</h3>
                 <h5><strong>Head Chair: Mischa Fritz</strong></h5>
                 <h5><strong>Vice Chairs: Lilac Peterson, Michael Nakada</h5></strong>
                 <h5><a href="topicSynopses/AIIB.pdf">Reducing the Incidence of the Unbanked, Developing Frameworks to Address Infrastructure Gap in Asia</a> |  <a href="http://aiib.blogs.bmun.org/">Blog</a> | <a href="mailto:treasurer@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee">
+            <div class=" committee">
                 <h3 class="committee-heading"> Arab League (THIMUN)</h3>
                 <h5><strong>Head Chair: Gloria Cheung</strong></h5>
                 <h5><strong>Vice Chairs: Zoe Brouns, Alexander Wilfert</h5></strong>
                 <h5><a href="topicSynopses/AL.pdf">Situation in Yemen, Protection of Cultural Property in Armed Conflict</a> |  <a href="http://al.blogs.bmun.org/">Blog</a> | <a href="mailto:gcheung@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee" style="text-align:right">
+            <div class=" committee" >
                 <h3 class="committee-heading"> European Union</h3>
                 <h5><strong>Head Chair: Pranay Patil</strong></h5>
                 <h5><strong>Vice Chairs: Brandon Doan, Danny Tuthill, Sarah Bauer</h5></strong>
                 <h5><a href="ComingSoon.php">Economic Impacts of Cyber Warfare, Treatment of Immigrants in the European Union</a> | <a href="http://eu.blogs.bmun.org/">Blog</a> | <a href="mailto:ppatil@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee">
+            <div class=" committee">
                 <h3 class="committee-heading"> Press Corps</h3>
                 <h5><strong>Head Chair: Natasha Cougoule</strong></h5>
                 <h5><strong>Vice Chairs: Sey Kim, Ashley Njoroge, Emma Lautanen, Hyunwook Kim</h5></strong>
                 <h5><a href="topicSynopses/PRESS.pdf">Open Agenda</a> | <a href="http://press.blogs.bmun.org/">Blog</a> | <a href="mailto:ncougoule@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee" style="text-align:right">
+            <div class=" committee" >
                 <h3 class="committee-heading"> The Community of Latin American and Caribbean States / La Comunidad de Estados Latinoamericanos y Caribeños (Spanish Bilingual)</h3>
                 <h5><strong>Head Chair: Brett Buckingham</strong></h5>
                 <h5><strong>Vice Chairs: Adrian Hernandez, Jane Kim</h5></strong>
-                <h5><a href="topicSynopses/CELAC.pdf">Economic Development Policy: Alternatives to Neoliberalism? (English), Preparing for Environmental Change:  Disaster Risk Management Strategies (Español) </a> |  <a href="http://celac.blogs.bmun.org/">Blog</a> | <a href="mailto:ops@bmun.org" target="_blank">Email</a></h5>
+                <h5><a href="topicSynopses/CELAC.pdf"> Extractive Industry and Export-Based Political Economy (English),  La Inversión Extranjera Directa (Español) </a> |  <a href="http://celac.blogs.bmun.org/">Blog</a> | <a href="mailto:ops@bmun.org" target="_blank">Email</a></h5>
             </div>
 
             <hr>
 
             <h2 class="bloc-heading">Crisis<small> Crisis Manager: Akshay Jangadeesh</small></h2>
 
-            <div class="col-md-6 committee">
+            <div class=" committee">
                 <h3 class="committee-heading"> Joint Cabinet Crisis</h3>
                 <h5><strong>Head Chair: Nate Parke</strong></h5>
                 <h5><strong>Vice Chairs: Jing Liong, Jessie Mao, Tushita Saraf, Michael Eliot</h5></strong>
-                <h5><a href="ComingSoon.php">East - West Relations During Perestroika</a> |  <a href="http://jcc.blogs.bmun.org/">Blog</a> | <a href="mailto:tech@bmun.org" target="_blank">Email</a></h5>
+                <h5><a href="topicSynopses/JCC.pdf">Perestroika: Reformation of the Soviet Economy</a> |  <a href="http://jcc.blogs.bmun.org/">Blog</a> | <a href="mailto:tech@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee" style="text-align:right">
+            <div class=" committee" >
                 <h3 class="committee-heading"> Tudor Court</h3>
                 <h5><strong>Head Chair: Eric Cherwin</strong></h5>
                 <h5><strong>Vice Chairs: Mekhala Hoskote, Arjun Banerjee</h5></strong>
                 <h5><a href="topicSynopses/TUDOR.pdf">Heir to England: Preservation of the House of Tudor</a> | <a href="http://tudor.blogs.bmun.org/">Blog</a> | <a href="mailto:echerwin@bmun.org" target="_blank">Email</a></h5>
             </div>
 
-            <div class="col-md-6 committee">
+            <div class=" committee">
                 <h3 class="committee-heading"> Berkeley Venture Capital</h3>
                 <h5><strong>Head Chair: Vishal Kalyanasundaram</strong></h5>
                 <h5><strong>Vice Chairs: Ben Sher, Jacky Tian</h5></strong>
